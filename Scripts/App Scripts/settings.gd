1449eece304cf12037ad87970fb59fe11fdeb599
Totally_RandomYT <68128408+TotsllyRandom@users.noreply.github.com>
2026-04-29 22:06:41 -0500

    working on fixing Settings bug

--- Scripts/App Scripts/settings.gd
+++ Scripts/App Scripts/settings.gd
@@ -5,23 +5,19 @@ var button
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	button = $VBoxContainer/DarkModeSettings/Button
-	button.set_block_signals(true)
 	button.button_pressed = GlobalSettings.dark_mode
-	button.set_block_signals(false)
-	GlobalSettings.dark_mode_changed.connect(fix_value)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if (GlobalSettings.dark_mode != button.button_pressed):
+		button.button_pressed = GlobalSettings.dark_mode
+
+
+func _on_button_pressed() -> void:
 	pass
 
-func fix_value(value):
-	button.set_block_signals(true)
-	button.button_pressed = value
-	button.set_block_signals(false)
 
-
-
-
-func _on_button_toggled(toggled_on: bool) -> void:
-	GlobalSettings.dark_mode = button.button_pressed
+func _on_button_up() -> void:
+	print("toggling from tab id ", get_parent().get_parent().ID)
+	#GlobalSettings.dark_mode = button.button_pressed
--- Scripts/text_color_manager.gd
+++ Scripts/text_color_manager.gd
@@ -9,10 +9,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	if GlobalSettings.dark_mode:
 		parent_node.add_theme_color_override("font_color", GlobalSettings.dark_mode_text)
-		parent_node.add_theme_color_override("font_hover", GlobalSettings.dark_mode_text)
 	else:
 		parent_node.add_theme_color_override("font_color", GlobalSettings.light_mode_text)
-		parent_node.add_theme_color_override("font_hover", GlobalSettings.light_mode_text)
